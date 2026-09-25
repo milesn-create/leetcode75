f01f188cf91cbb26e84e9fdfd5121e008b5a8b3f
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-25 21:43:50 +0300

    feat: add 283 Move Zeroes (Easy)

---
+++ two_pointers/283_move_zeroes.go
@@ -0,0 +1,15 @@
+package twopointers
+
+func MoveZeroes(nums []int) {
+	read, write := 0, 0
+	for read < len(nums) {
+		if nums[read] != 0 {
+			nums[write], nums[read] = nums[read], nums[write]
+			read++
+			write++
+		} else {
+			read++
+		}
+	}
+
+}
