dc9ec135fa41907a968635053bb324481af6011e
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-26 10:51:28 +0300

    feat: add 643 Maximum Average Subarray I (Easy)

---
+++ sliding_window/643_maximum_average_subarray_i.go
@@ -0,0 +1,16 @@
+package sliding_window
+
+func findMaxAverage(nums []int, k int) float64 {
+	maxSum := 0
+	sum := 0
+	for i := 0; i < k; i++ {
+		sum = sum + nums[i]
+	}
+	maxSum = sum
+	for i := k; i < len(nums); i++ {
+		sum = sum + nums[i] - nums[i-k]
+		maxSum = max(sum, maxSum)
+	}
+	return float64(maxSum) / float64(k)
+
+}
