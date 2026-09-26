78345c90bd9df00c3e37c86d5b48776510686c8a
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-26 11:23:11 +0300

    feat: add 1004 Max Consecutive Ones III (Medium)

---
+++ sliding_window/1004_max_consecutive_ones_iii.go
@@ -0,0 +1,20 @@
+package sliding_window
+
+func LongestOnes(nums []int, k int) int {
+	left := 0
+	countZ := 0
+	maxLen := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] == 0 {
+			countZ++
+		}
+		for countZ > k {
+			if nums[left] == 0 {
+				countZ--
+			}
+			left++
+		}
+		maxLen = max(maxLen, right-left+1)
+	}
+	return maxLen
+}
